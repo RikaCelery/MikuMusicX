20e69df22f76933b57e79b673fd3b8495bb0a770
RikaCelery <qincai233@gmail.com>
2024-12-26 12:17:33 +0800

    v0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         //noinspection EditedTargetSdkVersion
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1.3"
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
